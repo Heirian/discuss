745fb7fa1b00aedeaab37639bf40b7f8be3c0037
Heirian <shinkcc@gmail.com>
2018-04-23 02:48:23 +0000

    Updates ueberauth and adds credo

--- mix.exs
+++ mix.exs
@@ -38,8 +38,9 @@ defmodule Discuss.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:ueberauth, "~> 0.3"},
-     {:ueberauth_github, "~> 0.4"}]
+     {:ueberauth, "~> 0.4"},
+     {:ueberauth_github, "~> 0.7"},
+     {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
